3e886db2e664e697b4458c05175470022e2302f0
MunawarSaqibi <mohdmunawar.mar@gmail.com>
2026-07-27 16:22:02 +0530

    Express BoilerPlate

--- NodeDay2/index.js
+++ NodeDay2/index.js
@@ -2,11 +2,26 @@ const express= require("express")
 
 const app = express();
 
+// method - route
 app.get("/menu", (request,response)=>{
     console.log("GET request recieved on /menu route")
-    response.send("your request recevied , you will get response soon")
+    response.json({ resName: "Shah Ghouse"})
 })
 
+app.post("/menu", (request,response)=>{
+    console.log("Posted message")
+    response.json({message: "menu added successfully"})
+})
+
+app.put("/menu",(request,response)=>{
+    console.log("message updated successfully")
+    response.json({message: "menu updated successfully"})
+})
+
+
+app.delete("/menu",(request,response)=>{
+    response.json({message: "menu deleted successfully"})
+})
 app.listen("8080",()=>{
     console.log("server is listening on port 8080")
 })
