2399d6571f0c776cd979609a92902dc3ad80d2cf
MunawarSaqibi <mohdmunawar.mar@gmail.com>
2026-08-07 13:09:25 +0530

    AuthMiddleWare for token verification

--- NodeDay6/server.js
+++ NodeDay6/server.js
@@ -1,82 +1,90 @@
-const express=require("express")
+const express = require("express");
 const cors = require("cors");
-const jwt= require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
-const app= express();
+const app = express();
 
-const JWT_SECRET = "I Love Mandi"
+const JWT_SECRET = "I Love Mandi";
 
-let users=[]
+let users = [];
+app.use(express.json());
+app.use(cors());
 
-app.use(express.json())
-app.use(cors())
+const authMiddileware = (req, res, next) => {
+  const { token } = req.headers;
+  const jwtPayload = jwt.verify(token, JWT_SECRET);
 
-app.post("/signup",(req,res)=>{
-    const {username,password, email}=req.body;
-
-    const newUserObj={
-        username,
-        password,
-        email,
-    }
-    users.push(newUserObj);
-    console.log("current db status",users)
-
-res.json({
-    "msg": "User registered successfully",
-    "data": newUserObj
-})
-})
-
-app.post("/signin",(req,res)=>{
-
-    const {email,password}=req.body;
-
-    const foundUser = users.find((userObj)=>{
-        if(userObj.email==email && userObj.password == password){
-            return true
-        }
-    })
-
-    if(foundUser==undefined){
-        res.json({
-            "msg":"Inavlid login details or User Not Found"
-        })
-    }else{
-        const token = jwt.sign({username: foundUser.username}, JWT_SECRET)
-        res.json({
-            msg:"login Successfull",
-            token: token
-        })
-    }
-
-})
-
-app.get("/me",(req,res)=>{
-    const {token}=req.headers
-
-    const jwtPayload=jwt.verify(token, JWT_SECRET)
-
-    if(jwtPayload==undefined){
-        res.json({
-            "msg":"invalid credientials/ You are not allowed to access this data"
-        })
-    }
-
-
-    const verifiedUserData= users.find((userObj)=>{
-        if(userObj.username === jwtPayload.username){
-            return true
-        }
-    })
-
+  if (jwtPayload == undefined) {
     res.json({
-        "msg":"you are eligible to get data",
-        "data": verifiedUserData
-    })
-})
+      msg: "invalid credientials/ You are not allowed to access this data",
+    });
+  }
 
+  req.transferData = jwtPayload;
+  next();
+};
+app.post("/signup", (req, res) => {
+  const { username, password, email } = req.body;
 
-app.listen("8080",()=>{
-    console.log("server is listening at port 8080");
-})
+  const newUserObj = {
+    username,
+    password,
+    email,
+  };
+  users.push(newUserObj);
+  console.log("current db status", users);
+
+  res.json({
+    msg: "User registered successfully",
+    data: newUserObj,
+  });
+});
+
+app.post("/signin", (req, res) => {
+  const { email, password } = req.body;
+
+  const foundUser = users.find((userObj) => {
+    if (userObj.email == email && userObj.password == password) {
+      return true;
+    }
+  });
+
+  if (foundUser == undefined) {
+    res.json({
+      msg: "Inavlid login details or User Not Found",
+    });
+  } else {
+    const token = jwt.sign({ username: foundUser.username }, JWT_SECRET);
+    res.json({
+      msg: "login Successfull",
+      token: token,
+    });
+  }
+});
+
+app.use(authMiddileware);
+
+app.get("/me", (req, res) => {
+  const jwtPayload = req.transferData;
+
+  const verifiedUserData = users.find((userObj) => {
+    if (userObj.username === jwtPayload.username) {
+      return true;
+    }
+  });
+
+  res.json({
+    msg: "you are eligible to get data",
+    data: verifiedUserData,
+  });
+});
+
+app.get("/info", (req, res) => {
+  res.json({
+    msg: "AuthMiddleWare is used",
+  });
+});
+
+app.listen("8080", () => {
+  console.log("server is listening at port 8080");
+});
